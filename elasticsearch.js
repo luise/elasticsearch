94546b1dc980024381df9e689d138f1e44922944
Kevin Lin <kevin@quilt.io>
2017-08-03 17:09:33 -0700

    elasticsearch: Lint all files

--- elasticsearch.js
+++ elasticsearch.js
@@ -1,45 +1,43 @@
-const {Container, Service, publicInternet, PortRange} = require("@quilt/quilt");
+const { Container, Service, publicInternet, PortRange } = require('@quilt/quilt');
 
 function Elasticsearch(n) {
-    var ref = new Container("elasticsearch:2.4",
-        [
-            "--transport.tcp.port", this._transportPorts.min + "-" + this._transportPorts.max,
-            "--http.port", this.port.toString(),
-        ]
-    )
-    this.service = new Service("elasticsearch", ref.replicate(n));
+  const ref = new Container('elasticsearch:2.4',
+    [
+      '--transport.tcp.port', `${this.transportPorts.min}-${this.transportPorts.max}`,
+      '--http.port', this.port.toString(),
+    ]);
+  this.service = new Service('elasticsearch', ref.replicate(n));
 
-    if (n > 1) {
-        var hosts = this.service.children().join(",");
-        this.service.containers.forEach(function(c, i) {
-            c.command.push(
-                "--discovery.zen.ping.unicast.hosts", hosts,
-                "--network.host", "0.0.0.0"
-            );
-        });
-    }
+  if (n > 1) {
+    const hosts = this.service.children().join(',');
+    this.service.containers.forEach((c) => {
+      c.command.push(
+        '--discovery.zen.ping.unicast.hosts', hosts,
+        '--network.host', '0.0.0.0');
+    });
+  }
 
-    this.service.connect(this._transportPorts, this.service);
+  this.service.connect(this.transportPorts, this.service);
 }
 
-Elasticsearch.prototype.uri = function() {
-    return "http://" + this.service.hostname() + ":" + this.port;
-}
+Elasticsearch.prototype.uri = function uri() {
+  return `http://${this.service.hostname()}:${this.port}`;
+};
 
-Elasticsearch.prototype.public = function() {
-    publicInternet.connect(this.port, this.service);
-    return this;
-}
+Elasticsearch.prototype.public = function public() {
+  publicInternet.connect(this.port, this.service);
+  return this;
+};
 
-Elasticsearch.prototype.addClient = function(clnt) {
-    clnt.connect(this.port, this.service);
-}
+Elasticsearch.prototype.addClient = function addClient(clnt) {
+  clnt.connect(this.port, this.service);
+};
 
-Elasticsearch.prototype.deploy = function(depl) {
-    depl.deploy(this.service);
-}
+Elasticsearch.prototype.deploy = function deploy(depl) {
+  depl.deploy(this.service);
+};
 
-Elasticsearch.prototype._transportPorts = new PortRange(9300, 9400);
+Elasticsearch.prototype.transportPorts = new PortRange(9300, 9400);
 Elasticsearch.prototype.port = 9200;
 
 exports.Elasticsearch = Elasticsearch;
--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const {createDeployment, Machine} = require("@quilt/quilt");
-var Elasticsearch = require("./elasticsearch.js").Elasticsearch;
+const { createDeployment, Machine } = require('@quilt/quilt');
+const Elasticsearch = require('./elasticsearch.js').Elasticsearch;
 
-var clusterSize = 2;
+const clusterSize = 2;
 
-var deployment = createDeployment({});
-var baseMachine = new Machine({provider: "Amazon"});
+const deployment = createDeployment({});
+const baseMachine = new Machine({ provider: 'Amazon' });
 deployment.deploy(baseMachine.asMaster());
 deployment.deploy(baseMachine.asWorker().replicate(clusterSize));
 deployment.deploy(new Elasticsearch(clusterSize).public());
