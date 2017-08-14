2d74cdab3e298909971c946226ae826afa283f67
Kevin Lin <kevin@quilt.io>
2017-08-14 16:40:45 -0700

    blueprint: Use new Quilt API
    The Quilt API now deploys containers directly.

--- elasticsearch.js
+++ elasticsearch.js
@@ -1,23 +1,29 @@
-const { Container, Service, publicInternet, PortRange } = require('@quilt/quilt');
+const quilt = require('@quilt/quilt');
+
+function getHostname(c) {
+  return c.getHostname();
+}
 
 function Elasticsearch(n) {
-  const ref = new Container('elasticsearch:2.4',
-    [
+  const ref = new quilt.Container('elasticsearch', 'elasticsearch:2.4', {
+    command: [
       '--transport.tcp.port', `${this.transportPorts.min}-${this.transportPorts.max}`,
       '--http.port', this.port.toString(),
-    ]);
-  this.service = new Service('elasticsearch', ref.replicate(n));
+    ],
+  });
+  this.containers = ref.replicate(n);
+  this.service = new quilt.Service('elasticsearch-lb', this.containers);
 
   if (n > 1) {
-    const hosts = this.service.children().join(',');
-    this.service.containers.forEach((c) => {
+    const hosts = this.containers.map(getHostname).join(',');
+    this.containers.forEach((c) => {
       c.command.push(
         '--discovery.zen.ping.unicast.hosts', hosts,
         '--network.host', '0.0.0.0');
     });
   }
 
-  this.service.allowFrom(this.service, this.transportPorts);
+  quilt.allow(this.containers, this.containers, this.transportPorts);
 }
 
 Elasticsearch.prototype.uri = function uri() {
@@ -25,7 +31,7 @@ Elasticsearch.prototype.uri = function uri() {
 };
 
 Elasticsearch.prototype.allowFromPublic = function allowFromPublic() {
-  this.service.allowFrom(publicInternet, this.port);
+  quilt.allow(quilt.publicInternet, this.containers, this.port);
   return this;
 };
 
@@ -34,10 +40,11 @@ Elasticsearch.prototype.addClient = function addClient(clnt) {
 };
 
 Elasticsearch.prototype.deploy = function deploy(depl) {
+  depl.deploy(this.containers);
   depl.deploy(this.service);
 };
 
-Elasticsearch.prototype.transportPorts = new PortRange(9300, 9400);
+Elasticsearch.prototype.transportPorts = new quilt.PortRange(9300, 9400);
 Elasticsearch.prototype.port = 9200;
 
 exports.Elasticsearch = Elasticsearch;
